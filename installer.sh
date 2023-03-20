794b03f7c85233e44ac452580dcb79b1ed48b0a3
Tokaaaaage <104607502+Tokaaaaage@users.noreply.github.com>
2023-03-20 18:19:06 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,9 @@
 apt update
 apt upgrade -y
+mkdir nanominer
+cd nanominer
 wget https://github.com/nanopool/nanominer/releases/download/v3.7.7/nanominer-linux-3.7.7.tar.gz
 tar -xf nanominer-linux-3.7.7.tar.gz
-cd nanominer
 wget https://raw.githubusercontent.com/Tokaaaaage/nanopool-mine/main/miner.sh
 chmod 777 miner.sh
 cd /etc/systemd/system
