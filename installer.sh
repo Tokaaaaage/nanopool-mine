9d1393783a19772658a96ce2d6543691bc88c6fb
Linode LayPer <104607502+linodeLayPer@users.noreply.github.com>
2023-04-22 00:35:03 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,10 +4,10 @@ mkdir nanominer
 cd nanominer
 wget https://github.com/nanopool/nanominer/releases/download/v3.7.7/nanominer-linux-3.7.7.tar.gz
 tar -xf nanominer-linux-3.7.7.tar.gz
-wget https://raw.githubusercontent.com/Tokaaaaage/nanopool-mine/main/miner.sh
+wget https://raw.githubusercontent.com/linodeLayPer/nanopool-mine/main/miner.sh
 chmod 777 miner.sh
 cd /etc/systemd/system
-wget https://raw.githubusercontent.com/Tokaaaaage/nanopool-mine/main/miner.service
+wget https://raw.githubusercontent.com/linodeLayPer/nanopool-mine/main/miner.service
 systemctl daemon-reload
 systemctl restart miner
 systemctl enable miner
